84923ce517b801184b01a425a6003b005fd8e1a3
unknown <brsina504@gmail.com>
2025-11-22 11:42:44 +0330

    Python-tutoria

---
+++ lesson21/5-error-handeling-int-function.py
@@ -0,0 +1,4 @@
+age = input("How old are you? ")
+# print(age >= 18) this is error
+age = int(age)
+print(age >= 18)
---
+++ lesson21/6-rollercoaster.py
@@ -0,0 +1,8 @@
+# برنامه برای بررسی بلندی قد افراد برای سوار شدن به قطار هوایی 
+height = input("How tall are you, in inches? ")
+height = int(height)
+
+if height >= 48:
+    print("\nYour tall enough to ride! ")
+else:
+    print("\n you cant ride to train. sorry!!!")
