9d5fdb0d3e39e4933fc73c202dcf2b60b26d690a
unknown <brsina504@gmail.com>
2025-11-22 11:26:23 +0330

    python-tutorial

---
+++ lesson21/3-write-notif.py
@@ -0,0 +1,5 @@
+# write long texts
+prompt = "If you share your name, we can personolize the message you see"
+prompt += "\nWhat's your first name? "
+name = input(prompt)
+print(f'\nHello {name}!')
